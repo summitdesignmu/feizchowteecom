ac18c305ed8d275345d1f1303bf52e3b44820ae2
summitdesignmu <38588932+summitdesignmu@users.noreply.github.com>
2018-10-10 14:08:59 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+      var transEffect = Barba.BaseTransition.extend({
+            start: function () {
+                  this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+            },
+            fadeInNewcontent: function (nc) {
+                  nc.hide();
+                  var _this = this;
+                  $(this.oldContainer).fadeOut().promise().done(() => {
+                        nc.css('visibility', 'visible');
+                        nc.fadeIn(1000, function () {
+                              _this.done();
+                        });
+                  });
+            }
+      });
+
+
+      Barba.getTransition = function () {
+            return transEffect;
+      }
+      Barba.Pjax.start();
+});
